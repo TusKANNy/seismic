2c8a6bcf92662a4fb7ab0311cbad2f37c45e552c
leeeov4 <l.venuta@studenti.unipi.it>
2024-11-19 16:55:57 +0000

    Python interface updated

--- src/pylib/mod.rs
+++ src/pylib/mod.rs
@@ -1,5 +1,5 @@
 use crate::inverted_index::{
-    BlockingStrategy, ClusteringAlgorithm, Configuration, PruningStrategy, SummarizationStrategy,
+    BlockingStrategy, ClusteringAlgorithm, Configuration, PruningStrategy, SummarizationStrategy, Knn, KnnConfiguration
 };
 use crate::{InvertedIndex, SparseDataset};
 use half::f16;
@@ -15,6 +15,31 @@ pub struct PySeismicIndex {
 
 #[pymethods]
 impl PySeismicIndex {
+
+    #[getter]
+    pub fn get_dim(&self) -> PyResult<usize> {
+        Ok(self.inverted_index.dim())
+    }
+
+    #[getter]
+    pub fn get_len(&self) -> PyResult<usize> {
+        Ok(self.inverted_index.len())
+    }
+
+    #[getter]
+    pub fn get_nnz(&self) -> PyResult<usize> {
+        Ok(self.inverted_index.nnz())
+    }
+
+    #[getter]
+    pub fn get_is_empty(&self) -> PyResult<bool> {
+        Ok(self.inverted_index.is_empty())
+    }
+
+    pub fn print_space_usage_byte(&self) {
+        self.inverted_index.print_space_usage_byte();
+    }
+
     #[staticmethod]
     pub fn load(index_path: &str) -> PyResult<PySeismicIndex> {
         let serialized: Vec<u8> = fs::read(index_path).unwrap();
@@ -30,18 +55,46 @@ impl PySeismicIndex {
         println!("{:?}", r);
     }
 
+    // Build knn and add it to the inverted index
+    pub fn build_knn(&mut self, nknn: usize) {
+        let knn = Knn::new(&self.inverted_index, nknn);
+        self.inverted_index.add_knn(knn);
+    }
+
+    // Load an index, add a pre computed knn-graph to it and return the new index
+    #[staticmethod]
+    pub fn load_index_knn(index_path: &str, knn_path: &str, nknn: usize) -> PyResult<PySeismicIndex> {
+        println!("Loading ... {}", index_path);
+        let serialized: Vec<u8> = fs::read(index_path).unwrap();
+        let mut inverted_index = bincode::deserialize::<InvertedIndex<f16>>(&serialized).unwrap();
+        
+        let knn = Knn::new_from_path(knn_path.to_string(), nknn);
+
+        inverted_index.add_knn(knn);
+
+        inverted_index.print_space_usage_byte();
+
+        Ok(PySeismicIndex { inverted_index})
+    }
+
     #[staticmethod]
     #[allow(clippy::too_many_arguments)]
+    #[pyo3(signature = (input_file, n_postings, centroid_fraction, min_cluster_size, summary_energy, nknn=0, knn_path=None, batched_indexing=None))]
     pub fn build(
         input_file: &str,
         n_postings: usize,
         centroid_fraction: f32,
         min_cluster_size: usize,
         summary_energy: f32,
+        nknn: usize,
+        knn_path: Option<String>,
+        batched_indexing: Option<usize>,
     ) -> PyResult<PySeismicIndex> {
         let dataset = SparseDataset::<f32>::read_bin_file(input_file)
             .unwrap()
             .quantize_f16();
+
+        let knn_config = KnnConfiguration::new(nknn, knn_path);
 
         let config = Configuration::default()
             .pruning_strategy(PruningStrategy::GlobalThreshold {
@@ -55,7 +108,9 @@ impl PySeismicIndex {
                     doc_cut: 15,
                 },
             })
-            .summarization_strategy(SummarizationStrategy::EnergyPreserving { summary_energy });
+            .summarization_strategy(SummarizationStrategy::EnergyPreserving { summary_energy })
+            .knn(knn_config)
+            .batched_indexing(batched_indexing);
         println!("\nBuilding the index...");
         println!("{:?}", config);
 
