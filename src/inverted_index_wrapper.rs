2e9898e28c23c1ea42ced6ca20779b0b804ca1a3
leeeov4 <l.venuta@studenti.unipi.it>
2024-12-23 14:25:14 +0000

    tar/json support

--- src/inverted_index_wrapper.rs
+++ src/inverted_index_wrapper.rs
@@ -1,16 +1,20 @@
 use std::{
     collections::HashMap,
     fs::File,
-    io::{BufReader, Write},
+    io::{self, BufReader, BufRead},
     time::Instant,
 };
 
 use crate::json_utils::{extract_jsonl, JsonFormat};
 
+use indicatif::ProgressIterator;
 use itertools::Itertools;
 use serde::{Deserialize, Serialize};
 
 use serde_json::Deserializer;
+
+use flate2::read::GzDecoder;
+use tar::Archive;
 
 use crate::{
     inverted_index::Configuration, DataType, InvertedIndex, SpaceUsage, SparseDataset,
@@ -130,34 +134,24 @@ where
         self.remap_doc_ids(results, query_id) // return the documents remapped
     }
 
-    pub fn from_json(
-        json_path: &String,
-        config: Configuration,
+    pub fn process_data(
+        reader: BufReader<impl std::io::Read>,
+        row_count: usize,
         input_token_to_id_map: Option<HashMap<String, usize>>,
-    ) -> Self {
-        println!("Reading the collection..");
-        let f = File::open(json_path).expect(&format!("Unable to open {}", json_path));
-        let reader = BufReader::new(f);
-        let stream: serde_json::StreamDeserializer<
-            serde_json::de::IoRead<BufReader<File>>,
-            JsonFormat,
-        > = Deserializer::from_reader(reader).into_iter();
-
-        let start = Instant::now();
-
-        let mut doc_id_mapping = Vec::with_capacity(1_000_000);
-
-        let mut token_to_id_mapping = HashMap::<String, usize>::with_capacity(30000);
-
+    ) -> (SparseDataset<T>, Vec<String>, HashMap<String, usize>)
+    {
+        let mut doc_id_mapping = Vec::with_capacity(row_count);
+        let mut token_to_id_mapping = HashMap::<String, usize>::with_capacity(30_000);
         let mut converted_data = SparseDatasetMut::<T>::default();
-
-        let mut analog_counter = 0;
- 
-        for x in stream.into_iter() {
+    
+        let stream: serde_json::StreamDeserializer<_, JsonFormat> =
+            Deserializer::from_reader(reader).into_iter();
+    
+        for x in stream.into_iter().progress_count(row_count as u64) {
             let (doc_id, tokens, values) = extract_jsonl::<T>(x.unwrap());
             doc_id_mapping.push(doc_id);
-
-            let ids = match input_token_to_id_map {
+    
+            let ids: Vec<_> = match &input_token_to_id_map {
                 None => {
                     for token in tokens.iter() {
                         if !token_to_id_mapping.contains_key(token) {
@@ -165,40 +159,71 @@ where
                                 .insert(token.to_string(), token_to_id_mapping.len());
                         }
                     }
-                    let ids: Vec<u16> = tokens
-                        .iter()
-                        .map(|t| token_to_id_mapping[t] as u16)
-                        .collect();
-                    ids
+                    tokens.iter().map(|t| token_to_id_mapping[t] as u16).collect()
                 }
-
-                Some(ref valid_mapping) => {
-                    let ids: Vec<u16> = tokens.iter().map(|t| valid_mapping[t] as u16).collect();
-                    ids
+                Some(valid_mapping) => {
+                    tokens.iter().map(|t| valid_mapping[t] as u16).collect()
                 }
             };
-
+    
             let converted_vector: Vec<(u16, T)> = ids
                 .into_iter()
                 .zip(values)
                 .sorted_by(|(a, _), (b, _)| a.partial_cmp(&b).unwrap())
                 .collect();
-
+    
             converted_data.push_pairs(&converted_vector[..]);
-
-            if analog_counter % 10_000 == 0 {
-                print!("\rReading record {}", analog_counter);
-                let _ = std::io::stdout().flush();
-            }
-            analog_counter += 1;
         }
+    
+        let final_data: SparseDataset<T> = SparseDataset::<T>::from(converted_data);
+    
+        (final_data, doc_id_mapping, token_to_id_mapping)
+    }    
 
-        println!();
+    pub fn from_file(
+        file_path: &String,
+        config: Configuration,
+        input_token_to_id_map: Option<HashMap<String, usize>>,
+    ) -> Result<Self, io::Error> {
+
+        if file_path.ends_with(".jsonl") {
+            Ok(SeismicIndex::from_json(file_path, config, input_token_to_id_map))
+        } else if file_path.ends_with(".tar.gz") {
+            Ok(SeismicIndex::from_tar(file_path, config, input_token_to_id_map))
+        } else {
+            Err(io::Error::new(io::ErrorKind::InvalidInput, "Unsupported file type. Supported files: .jsonl, .tar.gz"))
+        }
+    }
+
+    pub fn from_json(
+        json_path: &String,
+        config: Configuration,
+        input_token_to_id_map: Option<HashMap<String, usize>>,
+    ) -> Self {
+        println!("Reading the collection..");
+        let start = Instant::now();
+
+        //read the file and count rows
+        let f = File::open(json_path).unwrap();
+        let reader = io::BufReader::new(f);
+        let row_count = reader.lines().count();
+
+        println!("Number of rows: {}", row_count);
+        println!(
+            "Elapsed time to read the number of rows {:} s",
+            start.elapsed().as_secs()
+        );
+
+        let f = File::open(json_path).expect(&format!("Unable to open {}", json_path));
+        let reader = BufReader::new(f);
+
+        let (final_data, doc_id_mapping, token_to_id_mapping) =
+            Self::process_data(reader, row_count, input_token_to_id_map);
+
         println!(
             "Elapsed time to read the collection {:}",
-            start.elapsed().as_micros()
+            start.elapsed().as_secs()
         );
-        let final_data: SparseDataset<T> = SparseDataset::<T>::from(converted_data);
 
         Self::new(
             final_data,
@@ -208,14 +233,51 @@ where
         )
     }
 
-    //TODO: implement
     #[allow(unused_variables)]
     pub fn from_tar(
-        json_path: &String,
+        tar_path: &String,
         config: Configuration,
         input_token_to_id_map: Option<HashMap<String, usize>>,
     ) -> Self {
-        todo!()
+        println!("Reading the collection..");
+        let start = Instant::now();
+
+        //decompress gz, extract first file (json) of the archive
+        //read the file and count rows
+        let tar_gz_file = File::open(tar_path).expect(&format!("Unable to open {}", tar_path));
+        let gz_decoder = GzDecoder::new(tar_gz_file);
+        let mut archive = Archive::new(gz_decoder);
+        let json_entry = archive.entries().unwrap().next().unwrap().unwrap();
+        let reader = BufReader::new(json_entry);
+        let row_count = reader.lines().count();
+
+        println!("Number of rows: {}", row_count);
+        println!(
+            "Elapsed time to read the number of rows {:} s",
+            start.elapsed().as_secs()
+        );
+
+        //Deserialize json
+        let tar_gz_file = File::open(tar_path).expect(&format!("Unable to open {}", tar_path));
+        let gz_decoder = GzDecoder::new(tar_gz_file);
+        let mut archive = Archive::new(gz_decoder);
+        let json_entry = archive.entries().unwrap().next().unwrap().unwrap();
+        let reader = BufReader::new(json_entry);
+
+        let (final_data, doc_id_mapping, token_to_id_mapping) =
+            Self::process_data(reader, row_count, input_token_to_id_map);
+
+        println!(
+            "Elapsed time to read the collection {:}",
+            start.elapsed().as_secs()
+        );
+        
+        Self::new(
+            final_data,
+            config,
+            Some(doc_id_mapping),
+            token_to_id_mapping,
+        )
     }
 
     pub fn print_space_usage_byte(&self) {
@@ -244,6 +306,10 @@ where
 
     pub fn add_knn(&mut self, knn: Knn) {
         self.inverted_index.add_knn(knn);
+    }
+
+    pub fn knn_len(&self) -> usize {
+        self.inverted_index.knn_len()
     }
 
 }
